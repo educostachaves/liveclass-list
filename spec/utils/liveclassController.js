afe820d4df21b1d8bd70ac2e12adcf684f9c14bf
Eduardo Chaves <educostachaves@hotmail.com>
2015-06-11 19:12:31 -0300

    Starting with Js Unity Tests

---
+++ spec/utils/liveclassController.js
@@ -0,0 +1,71 @@
+describe('app', function () {
+    
+    var scope, controller;
+    
+    beforeEach(function () {
+        module('app');
+    });
+
+    describe('liveclassController', function () {
+        beforeEach(inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+            controller = $controller('liveclassController', {
+                '$scope': scope
+            });
+        }));
+        
+        it('Rows Json is Fine', function () {
+
+            var obj = {
+                "Url": "",
+                "Id": null,
+                "ChatUrl": "",
+                "HomeworkUrl": "",
+                "Name": "",
+                "Disciplines": [{}],
+                "Teachers": [{}],
+                "ExtraContentUrl": "",
+                "Start": "",
+                "Duration": "",
+                "IsFree": null,
+                "IsTrial": null,
+                "Tag": "",
+                "Route": "",
+                "Status": null
+            };
+
+            expect(obj).toEqual('Eduardo');
+
+        });
+
+    });
+    
+});
+
+// Runner
+(function () {
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 1000;
+
+    var htmlReporter = new jasmine.HtmlReporter();
+
+    jasmineEnv.addReporter(htmlReporter);
+
+    jasmineEnv.specFilter = function (spec) {
+        return htmlReporter.specFilter(spec);
+    };
+
+    var currentWindowOnload = window.onload;
+
+    window.onload = function () {
+        if (currentWindowOnload) {
+            currentWindowOnload();
+        }
+        execJasmine();
+    };
+
+    function execJasmine() {
+        jasmineEnv.execute();
+    }
+
+})();
